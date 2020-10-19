f9e417ac4ab1bdf9d482599611008bb10022fb00
BardoftheOzarks <1886@holbertonschool.com>
2020-10-19 14:41:21 +0000

    added task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,24 @@
+#include "sort.h"
+/**
+ * bubble_sort - A bubble sort algorithm
+ * @array: an array of integers
+ * @size: the size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int master, swapper, temp;
+
+	for (master = 0; master < size - 1; master++)
+	{
+		for (swapper = 0; swapper < ((size - master) - 1); swapper++)
+		{
+			if (array[swapper] > array[swapper + 1])
+			{
+				temp = array[swapper];
+				array[swapper] = array[swapper + 1];
+				array[swapper + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
